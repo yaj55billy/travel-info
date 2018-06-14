cea05dc9f63fbf1fcaa82ea24f84b52a84c6e3b2
紀冠逸 <yaj55ushydna@gmail.com>
2018-06-14 12:07:59 +0800

    js rwd

--- js/all.js
+++ js/all.js
@@ -1,25 +1,8 @@
 
 
-
-
 var xhr = new XMLHttpRequest();
 
-//readyState 
-//0 創建了XMLHttpRequest，但並沒有連結資料
-//1 open 設定了資料，但還沒有傳送資料
-//4 撈到資料，數據完全接收
-
-//補充 2表示偵測到 send   3表示loading中，有的檔案內容很大
-
-// get post
-//網址
-// 同步和非同步
 xhr.open('get','https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97',true);
-
-// 同步和非同步
-// true 非同步: 不會等資料傳回來，就會繼續往下跑，等到回傳才會自動回傳
-// false 同步: 會等資料傳回來，才繼續往下跑
-// 較常使用ture的概念，像是現在常用的框架都有非同步概念
 
 xhr.send(null);
 
@@ -32,11 +15,24 @@ xhr.onload = function(){
     //將地區選擇存取，如果發生change事件則執行areaFilter
     var location = document.getElementById('location');
     location.addEventListener('change',areaFilter);
+    location.addEventListener('change',bodySlider);
+
+    // 點擊熱門區域，click後執行areaFilter
+    var hotBtn = document.querySelectorAll('.hot-list li');
+    for(var i = 0 ; i<hotBtn.length; i++){
+        hotBtn[i].addEventListener('click',hotFilter);
+        hotBtn[i].addEventListener('click',bodySlider);
+    }
+
+    var contentTitle = document.querySelectorAll('.content-title');
 
     //location 篩選
-    function areaFilter(){
-        var locationValue = document.getElementById('location').value;
+    function areaFilter(e){
         // console.log(locationValue); //選到就印出那個區域
+        var locationValue = document.getElementById('location').value;
+        
+        // 抓取li的文字
+        var hotFilterArea = e.target.textContent;
 
         var filterData =[];
 
@@ -50,37 +46,133 @@ xhr.onload = function(){
                 //filterData 為經過篩選的資料 +入陣列
                 filterData.push(data.result.records[i]);   
             }
+
+            if(hotFilterArea == dataArea){
+                filterData.push(data.result.records[i]);  
+            }
         }
-        updateContent(filterData,locationValue);
+
+        for(var i =0; i<contentTitle.length; i++){
+            contentTitle[i].textContent = locationValue;
+        }
+        pageChange(filterData);
     }
 
+    function hotFilter(e){
+        // console.log(locationValue); //選到就印出那個區域
+        var locationValue = document.getElementById('location').value;
+        
+        // 抓取li的文字
+        var hotFilterArea = e.target.textContent;
+
+        var filterData =[];
+
+        // dataLen為100筆，在這100筆中撈出條件符合的
+        for (var i = 0; i< dataLen; i++) {
+
+            //存取 dataArea 為這個物件的區域
+            var dataArea = data.result.records[i].Zone;
+
+            if(hotFilterArea == dataArea){
+                filterData.push(data.result.records[i]);  
+            }
+        }
+
+        for(var i =0; i<contentTitle.length; i++){
+            contentTitle[i].textContent = hotFilterArea;
+        }
+        pageChange(filterData);
+    }
+
+    function bodySlider(){
+        $('html,body').animate({scrollTop:$('#content').offset().top},1000);
+    }
 }
+
+// 頁碼
+var page = document.querySelector(".page_no");
+
+var pageNum = 6;
+var nowPage = 1;
+
+function pageChange(array){
+    // console.log(array);
+    var result = [];
+    var btnStr = "";
+
+    for(var i=0 ; i<array.length ; i+=pageNum){
+
+      result.push(array.slice(i,i+pageNum));
+      console.log(result.length);
+      btnStr += '<button class="page"><a href="#">'+ result.length +'</a></button>';
+    }
+    
+    page.innerHTML = '<button class="page_prev"><a href="#"><p> « </p></a></button>'+'<div class="page_ul">'+ btnStr +'</div>' + '<button class="page_next"><a href="#"><p> » </p></a></button>';
+
+
+    // 如果總資料超過每頁上限 顯示頁數
+    if (array.length > pageNum){
+        page.style.display = "block";
+      
+    }else{
+        page.style.display = "none";
+    }
+
+    var startInfo = (nowPage-1) * pageNum + 1; 
+    //開始顯示的資料
+    var endInfo = nowPage * pageNum; 
+    //最後顯示的資料
+    
+    page.addEventListener('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({scrollTop:$('#content').offset().top},1000);
+        var num = e.target.textContent;
+        startInfo = (num-1) * pageNum + 1;
+        endInfo = num * pageNum;
+        updateContent(startInfo,endInfo,array);
+    });
+
+    if(nowPage ==1){
+        console.log('第一頁');
+    }
+
+    console.log(array);
+    updateContent(startInfo,endInfo,array);
+    
+  }
 
 
 var content = document.querySelector('.content-list');
 
+
 // 組字串
+function updateContent(startInfo,endInfo,array){
 
-function updateContent(filterData,locationValue){
-
-    console.log(filterData);
-    console.log(filterData.length);
-
-    var str = '';
-    var contentTitle = document.querySelector('.content-title');
-    contentTitle.textContent = locationValue;
+    console.log(startInfo);
+    console.log(endInfo);
     
-    for (var i = 0; i< filterData.length; i++) {
+    var str = '';
+
+    // 如果資料未滿4筆時，則將endInfo帶入陣列長度，例如array只有2筆資料
+    // ，但如果endInfo為4時，則for迴圈跑的第2、第3筆將會出錯
+    if(array.length < endInfo){
+        endInfo = array.length;
+    }
+    
+    for (var i =startInfo-1; i< endInfo; i++) {
+        console.log(array[i].Picture1);
+        console.log(array[i].Name);
+        
         str = str +  `
         <li class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
             <div class="list">
-                <div class="list-img" style="background-image: url(${filterData[i].Picture1})">
+                <div class="list-img" style="background-image: url('${array[i].Picture1}')">
                     <div class="list-img-text">
                         <h3>
-                            ${filterData[i].Name}
+                            ${array[i].Name}
                         </h3>
                         <div class="lit-area">
-                            ${filterData[i].Zone}
+                            ${array[i].Zone}
                         </div>
                     </div>
                 </div>
@@ -88,15 +180,15 @@ function updateContent(filterData,locationValue){
                     <ul>
                         <li class="" id="list-time">
                             <img src="images/icons_clock.png" alt="">
-                            ${filterData[i].Opentime}
+                            ${array[i].Opentime}
                         </li>
                         <li class="" id="list-addr">
                             <img src="images/icons_pin.png" alt="">
-                            ${filterData[i].Add}
+                            ${array[i].Add}
                         </li>
                         <li class="" id="list-phone">
                             <img src="images/icons_phone.png" alt="">
-                            ${filterData[i].Tel}
+                            ${array[i].Tel}
                         </li>
                     </ul>
                     <div>免費參觀</div>
@@ -107,6 +199,10 @@ function updateContent(filterData,locationValue){
     content.innerHTML = str;
 
 }
+
+
+
+
 
 
 
